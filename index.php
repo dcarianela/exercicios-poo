0c5d9649e67b5a08b37a80e051e0a196edb47043
dcarianela <aki.hayakawa5577@gmail.com>
2025-04-15 10:27:13 -0300

    exercicio 02

--- index.php
+++ index.php
@@ -3,21 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício 01</title>
+    <title>Exercício 02</title>
 </head>
 <body>
-    <h1>Exercício 01</h1>
+    <h1>Exercício 02</h1>
     <hr>
     <p><i>Faça as chamadas e testes nesta página</i></p>
 
 <?php
 require_once "src/Livro.php";
 
-$livro = new Livro();
-
-$livro->setTitulo("Divinos Rivais");
-$livro->setAutor("Rebeca Ross");
-$livro->setPaginas(464);
+$livro = new Livro("Divinos Rivais", "Rebeca Ross", 464);
 ?>
 
     <h2>Acessando/lendo os dados do objeto</h2>
